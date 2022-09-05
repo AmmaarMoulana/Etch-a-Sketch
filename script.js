0b024e9d59e474c9ed6c0958226acf93108c7d28
Ammaar <ammarmoulana@gmail.com>
2022-09-05 12:58:32 +0530

    updated grid change option

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-for ( i=0; i<256; i++) {
-    const divs = document.querySelector('#container')
-    const innerdivs = document.createElement('div')
-    innerdivs.classList.add('gridboxes')
-    divs.appendChild(innerdivs)
+
+function createGrid (rows) {
+
+    
+
+    for ( i=0; i< rows * rows; i++) {
+        const innerdivs = document.createElement('div')
+        innerdivs.classList.add('gridboxes')
+        container.appendChild(innerdivs)
+
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+
+        innerdivs.addEventListener('mouseover', e => {
+        innerdivs.style.backgroundColor = "black"
+        })
+
+       
+        reset.addEventListener('click', () => {
+        innerdivs.style.backgroundColor = "white"
+        })
+    
+
+
+    }
+    
+    
 }
+
 
 const box = document.querySelectorAll('.gridboxes')
 
 
-function changeGrid (choice) {
-    for ( i=0; i<choice*choice; i++) {
-        const divs = document.querySelector('#container')
-        const innerdivs = document.createElement('div')
-        innerdivs.classList.add('gridboxes')
-        divs.appendChild(innerdivs)
-        console.log(choice)
-        
-    }
-}
-
-
-
-
-for (const gridboxes of box) {
-    gridboxes.addEventListener('mouseover', () => {
-        gridboxes.style.backgroundColor = "black"
-    })
-}
-
-
-reset.addEventListener('click',() => {
-    for (const gridboxes of box) {
-        gridboxes.style.backgroundColor = "white"
-    }
-})
-
-
 gridChoice.addEventListener('click', () => {
     let choice = prompt("Choose a number of boxes per row", "")
-    changeGrid(choice)
+    container.style.gridTemplateRows = `repeat(${choice}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${choice}, 1fr)`;
+    createGrid(choice)
 
 })
 
-
-
+createGrid(16)
